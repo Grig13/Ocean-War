3590e56ed37674831277f7c34052787ca35882dc
Grig13 <72690365+Grig13@users.noreply.github.com>
2021-04-20 20:57:41 +0300

    Add files via upload
    Implemented: 
    -menu background + logo.
    - buttons, text + buttons functionality
    -separate file for the game

--- Game.py
+++ Game.py
@@ -1,2 +1,33 @@
 import pygame
 import sys
+
+pygame.init()
+
+clock = pygame.time.Clock()
+
+screen_width = 1280
+screen_height = 720
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Ocean War")
+
+menu_background = pygame.image.load('ocean_wallpaper(unfinished).png')
+
+click = False
+
+
+
+def game():
+    running = True
+    screen.fill((0,0,0))
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+                    running = False
+        pygame.display.update()
+        clock.tick(30)
